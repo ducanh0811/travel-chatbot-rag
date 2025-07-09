2baf3d1a9abf757c0114ba148e75e338465bf77c
Lopez <dongoalong86@gmail.com>
2025-07-09 11:06:35 +0700

    Output parser

--- travel_information_agent.py
+++ travel_information_agent.py
@@ -5,14 +5,14 @@ from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
 load_dotenv()
 def create_information_agent():
-    llm = ChatOpenAI(model="gpt-3.5-turbo")
+    llm = ChatOpenAI(model="gpt-3.5-turbo",temperature=0)
     tools = [rag_tool]
 
     # system prompt có biến user_input
     prompt = """
 Bạn là agent cung cấp thông tin về Du Lịch Đà Nẵng trong ChromaDB.
 - Dùng rag_tool khi khách hàng muốn hỏi về các địa điểm du lịch, cafe, nhà hàng, khách sạn, và đừng gộp các địa điểm này trong câu trả lời.
-- Giữ nguyên output của tool
+- Giữ nguyên output của rag_tool
 User hỏi: {user_input}
 """
 
@@ -31,7 +31,7 @@ User hỏi: {user_input}
 if __name__ == "__main__":
     load_dotenv()
     weather_agent = create_information_agent()
-    user_query = "Gợi ý nhà hàng ở Quận Sơn Trà"
+    user_query = "Gợi ý các địa danh du lịch."
     response = weather_agent.invoke({"messages": {"role": "user", "content": user_query}})
     # In kết quả
     for msg in response["messages"]:
